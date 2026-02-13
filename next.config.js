b73be988bf456f7abbdef74dcb5ef8ceb550bd1a
owner <owner@MacBookPro.lan>
2026-02-13 11:00:15 -0500

    Configure Next.js for static export to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  trailingSlash: true,
+}
 
 module.exports = nextConfig
 
